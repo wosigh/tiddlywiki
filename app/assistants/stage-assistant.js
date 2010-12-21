06325843671453e8401ff39705e0ddcab7c5c156
Thomas Linden <scip@k2.daemon.de>
2010-12-21 17:17:13 +0100

    fixed git error

--- app/assistants/stage-assistant.js
+++ app/assistants/stage-assistant.js
@@ -5,3 +5,8 @@ function StageAssistant () {
 StageAssistant.prototype.setup = function() {
     this.controller.pushScene("main");
 }
+StageAssistant.prototype.handleCommand = function(event) {
+      if(event.type == Mojo.Event.back) {
+              event.stop();              
+      }  
+}
